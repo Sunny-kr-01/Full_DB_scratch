37c64415072bde726e021f6ee63c7194250d7702
Sunny-kr-01 <2405984@kiit.ac.in>
2025-12-24 01:41:10 +0530

    Schema created successfully

--- index.js
+++ index.js
@@ -12,4 +12,25 @@ mongoose.connect('mongodb://127.0.0.1:27017')
     console.log("Mongo error : ",err);
 })
 
-
+const users_schema=new mongoose.Schema({
+    first_name:{
+        type:String,
+        required:true,
+    },
+    last_name:{
+        type:String,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    gender:{
+        type:String,
+        required:true,
+    },
+    job_title:{
+        type:String,
+        required:true,
+    }
+})
