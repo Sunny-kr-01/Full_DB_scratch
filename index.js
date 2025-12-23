e5238256b60f5a3a9a41662382c3c8d6aeff55ef
Sunny-kr-01 <2405984@kiit.ac.in>
2025-12-24 01:33:12 +0530

    MongoDB conected

--- index.js
+++ index.js
@@ -2,3 +2,14 @@ const express= require('express')
 const mongoose=require('mongoose')
 
 const app= express();
+
+//mongoose.connect is a promise
+mongoose.connect(' mongodb://127.0.0.1:27017')
+.then(()=>{
+    console.log("MongoDB connected")
+})
+.catch((err)=>{
+    console.log("Mongo error : ",err);
+})
+
+
