916eb67b41bcc72fa1f9ee808d60105e6402cf50
Sunny-kr-01 <2405984@kiit.ac.in>
2025-12-24 01:40:44 +0530

    Schema created

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mongoose=require('mongoose')
 const app= express();
 
 //mongoose.connect is a promise
-mongoose.connect(' mongodb://127.0.0.1:27017')
+mongoose.connect('mongodb://127.0.0.1:27017')
 .then(()=>{
     console.log("MongoDB connected")
 })
