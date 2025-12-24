ead47163c384516c3925563f825f4dc0aed8e021
Sunny-kr-01 <2405984@kiit.ac.in>
2025-12-24 20:09:06 +0530

    get logic used async await to convert my_users to array uses (.fing())

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express= require('express')
 const mongoose=require('mongoose')
 
+const PORT=67;
 const app= express();
 
+app.use(express.urlencoded({extended:false}))
+
 //mongoose.connect is a promise
-mongoose.connect('mongodb://127.0.0.1:27017')
+mongoose.connect('mongodb://127.0.0.1:27017/Revison')
 .then(()=>{
     console.log("MongoDB connected")
 })
@@ -37,3 +40,45 @@ const users_schema=new mongoose.Schema({
 
 // MODEL
 const my_users=mongoose.model('Users',users_schema);
+
+app.route('/json/users')
+.get((req,res)=>{
+    res.json({status : "Pending"})
+})
+.post(async(req,res)=>{
+    const body=req.body;
+    const result=await my_users.create(
+        {
+            first_name : body.first_name,
+            last_name : body.last_name,
+            email : body.email,
+            gender : body.gender,
+            job_title : body.job_title,
+        }
+    )
+    res.status(200).json({status : "Success"})
+})
+
+app.get('/',(req,res)=>{
+    res.end("Home Page")
+})
+app.get('/users',async(req,res)=>{
+    const users=await my_users.find();
+    const html=`
+    <ul>
+    ${users.map((user)=>{
+        return `<li>${user.first_name}</li>`
+    }).join("")
+}
+    </ul>
+    `
+    res.send(html)
+})
+app.get('/json',(req,res)=>{
+    res.end("For json file")
+})
+
+
+app.listen(PORT,()=>{
+    console.log(`Server is listening on port ${PORT}`)
+})
